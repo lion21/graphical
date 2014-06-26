792af9b29704da696568c8b0df4d66c58df52961
lion21 <lion21as@fullsail.edu>
2014-06-26 19:35:50 -0400

    redo commit

---
+++ README.txt
@@ -0,0 +1,121 @@
+README.txt
+
+Profile Page Deployement
+
+1. Install Git on Laptop
+	a. www.macports.org
+	b.open terminal on laptop
+
+		$sudo port install git-core +svn +doc +bash_completion +gitweb
+
+		The basic git cms begin as
+		
+		$git config
+
+		--global
+
+		. /etc/gitconfig   -----contains values for evrey user on the sysand thier  
+
+								repos	
+
+
+
+
+		C. Set up your username and email ---git uses this info for every commit
+		
+		$git congig --global user.name "yourusername"
+
+		$git config --global user.email youremailaddress
+
+
+		D. Set up Editor
+
+		$git config --global core.editor editorname
+
+		E. Set up Diff Tool
+			see ch 7 for custom 	
+
+
+		F. Check Settings
+		
+		$git config --list
+
+		$git help < verb >
+
+		$git < verb > --help
+
+		$git help config 
+
+
+2. To clone in laptop
+	
+	open terminal on mac
+
+	$git clone <https:// address here from your repo on git hub>
+
+	this will immediatley download your clone
+
+	$ ls      to list your files
+
+	$ cd     your folder
+
+	to edit readme.tx if there is one 
+
+	$ pico readme.txt
+
+	this opens the editor in terminal and you can edit the readme file	
+
+	after edit is complete run the following to commit back up to github
+
+     $ git add -A
+     
+     $ git commit -m "update readme file"
+     [master 5aefef9] update readme file
+     2 files changed, 22 insertions(+), 1 deletion(-)
+     create mode 160000 graphicalGraffiti
+
+     $ git push
+
+     warning: push.default is unset; its implicit value is changing in
+     Git 2.0 from 'matching' to 'simple'. To squelch this message
+     and maintain the current behavior after the default changes, use:
+
+     git config --global push.default matching
+
+     To squelch this message and adopt the new behavior now, use:
+
+     git config --global push.default simple
+
+    See 'git help config' and search for 'push.default' for further information.
+    (the 'simple' mode was introduced in Git 1.7.11. Use the similar mode
+    'current' instead of 'simple' if you sometimes use older versions of Git)
+
+
+   Username for 'https://github.com': lion21
+
+  Password for 'https://lion21@github.com': 
+  
+Counting objects: 5, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (3/3), 591 bytes | 0 bytes/s, done.
+Total 3 (delta 1), reused 0 (delta 0)
+To https://github.com/lion21/graphicalGraffiti.git
+   2b188fd..5aefef9  master -> master
+Susans-MacBook-Pro:graphicalGraffiti susantodd$ 
+
+
+
+	$git status
+	$git add -A
+	$commit -m 'fixed that problem'
+	$git push prodServer master	
+	$git remote add prodServer ssh://myusername @ xxxx.xxxx.xxxx.xxxx/var/repos/mobilDev.git
+	$enter your password
+
+
+
+
+
+
+								
---
+++ js/main.js
@@ -0,0 +1,153 @@
+/*  
+Susan Todd
+
+*/
+/*==========ToolTip===========================*/
+(function($){
+	console.log("ready");//ready
+
+
+	$(".masterTooltip").hover(function()   {
+
+		var title=$(this).attr("title");//setting title = current "title"
+
+		$(this).data("tipText", title)
+			   .removeAttr("title");
+
+		$('<p class="tooltip"></p>').text(title)
+									.appendTo("body")//append the title info to the body
+									.fadeIn("slow")  ;
+
+		//close hover, chain the hover out and remove tooltip fn						
+	}, function(){
+
+		//hover out and remove from page after use
+		$(this).attr("title",  $(this).data("tipText") );
+
+		$(".tooltip").remove();
+
+		//chain mousemove for cursor position
+	}).mousemove (function(e){
+
+		var mousex=e.pageX + 20;//getting x-coordinate on pg-horizontal
+		var mousey=e.pageY +10;//y co-ordiante-vertical
+
+		$(".tooltip").css({top:mousey, left:mousex})
+						 
+
+	});//close tooltip fn
+
+/*+++++++++++BUTTONS++++++++++++++++++++++++++++++++++++++++++++*/
+
+   //take user from project page back to the dashboard/admin page
+   $("#homeBtn").on("click", function(e){
+   	  //homeBtn 
+		e.preventDefault();
+
+		window.location.assign("index.html");//go from projects.html back to dashboard
+    });
+
+
+	$("#coffeeBtn").on("click", function(e){
+		
+		e.preventDefault();
+
+		window.location.assign("coffee.html");
+	});//coffeeBtn" 
+
+
+	$("#contactBtn").on("click", function(e){
+		
+		e.preventDefault();
+
+		window.location.assign("contact.html");
+	});// "contactBtn"
+
+
+	$("#404Btn").on("click", function(e){
+		//class="taskbtn" >
+		e.preventDefault();
+
+		window.location.assign("404.html");
+	});// "404Btn"
+
+
+/*===========Accordian for Admin/Dashboard Page=============*/
+		
+  $("ul.tabs").each(function() {
+	//keeping track of the active tab
+	//context of $(this) = $active  //find anchors
+	var $active,$content,$links=$(this).find('a');
+
+    //if location.hash matches one of the links then make it active
+    //if not then use the first tab as the active tab
+
+    //we will hide the content tabs thtat are not non-active           //setting tabs at index 0
+
+    $active = $($links.filter( '[href= "  '+location.hash+'  " ]' )[0]   || $links[0] );
+
+    //adding class .active
+    $active.addClass('active');
+
+
+    $content = $($active[0].hash);
+
+
+    //hide non-active info from other 2 tabs
+    $links.not($active).each(function(){ 
+		//this anochor hide it
+	    $(this.hash).hide();
+    });//close links not active
+
+
+
+     //Bind this to  a click event
+	$(this).on('click','a' , function(e){
+
+	   //remove class active after use
+	   $active.removeClass('active');
+	   $content.hide();
+
+
+       //updating tabs w/ active class 
+	   $active = $(this);
+	   $content = $(this.hash);
+
+
+       //make tab active and show the content
+	   $active.addClass('active');
+	   $content.show();
+
+	   //prevent anchor defaault click action
+	   e.preventDefault();
+ 
+	});//close click event
+ 
+});//close tabs 
+
+/*============select a status for projects=========*/
+
+$("#selectable").selectable();
+$("#projectstatus").buttonset();
+//must add the ids to my html for the jq-ui to work
+
+/*=========Sortable from JQuery UI=================*/
+
+//allows you to sort the projects on the projects page
+ $( "#sortable" ).sortable();
+ $( "#sortable" ).disableSelection();
+  
+
+/*==========draggable/drppable===================*/
+//code for dragging  and dropping currently applied to images
+
+$(function() {
+	$("#draggable").draggable();
+	//$("#droppable").droppable();
+		
+});
+	
+
+})(jQuery); // end private scope
+
+
